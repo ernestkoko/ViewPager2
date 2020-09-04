c93ea56c06e8fd93dec16c610fa224cadc0d0c5b
ernestkoko <ernesteferetin@yahoo.com>
2020-09-04 20:55:08 +0100

    Initial commit

---
+++ app/src/main/java/com/ernestkoko/viewpager/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ernestkoko.viewpager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //hide the action bar
+        supportActionBar?.hide()
+    }
+}
